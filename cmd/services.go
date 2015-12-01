1acb62ba2aa7cfb3683eb8bb0065a8aef3ccf8cd
jksun <sjkyspa@gmail.com>
2015-12-02 02:04:57 +0800

    add marathon

--- cmd/services.go
+++ cmd/services.go
@@ -2,9 +2,31 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/gambol99/go-marathon"
 )
+
 
 func ServiceCreate() error {
 	fmt.Println("create service")
+
+	marathonURL := "http://marathon.tw.com"
+	config := marathon.NewDefaultConfig()
+	config.URL = marathonURL
+	client, err := marathon.NewClient(config)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("connected to marathon")
+
+	application := marathon.NewDockerApplication()
+	application.Name("gocd111").CPU(1).Memory(441).Storage(0)
+	application.Container.Docker.Container("registry.tw.com:80/gocd-server").Bridged().ExposePort(8153, 0, 0, "tcp")
+	applicationCreated, err := client.CreateApplication(application)
+
+	if err != nil {
+		return err
+	}
+	fmt.Println(applicationCreated.ID)
 	return nil
 }
