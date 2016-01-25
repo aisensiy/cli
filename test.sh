d1c2f978fd42fd79f284430b5f12c1eea1534cfa
hjliu <hjliu@thoughtworks.com>
2016-01-25 10:49:31 +0800

    add test for `routes:unbind` cmd; refine stack name

--- test.sh
+++ test.sh
@@ -167,40 +167,46 @@ appName="app`date +%s`"
 stackName="stack`date +%s`"
 domainName="domain.`date +%s`.com"
 routePath="path/`date +%s`"
-sshFilePath="~/.ssh/id_rsa.pub"
 
 ${cde} register http://192.168.50.4:31088 --email $user_name@tw.com --password admin 2>&1\
 | watchError | report "register"
 ${cde} login http://192.168.50.4:31088 --email $user_name@tw.com --password admin\
  2>&1 | watchError | report "login"
 ${cde} whoami 2>&1 | watchWhoamI | report "whoami"
+
 ${cde} stacks:create ${stackName} 2>&1 | watchError | report "stacks:create"
 ${cde} stacks:list 2>&1 | watchStack | report "stacks:list"
 ${cde} stacks:remove  2>&1 | watchError | report "stacks:remove"
+
 sshKeyId=`${cde} keys:add ~/.ssh/id_rsa.pub 2>&1 | watchNewKey `; echo $sshKeyId| report "keys:add"
 if [[ ${sshKeyId} == "Fail" ]]; then
   exit 1
 fi
 ${cde} keys:list 2>&1 | watchKeys | report "keys:list"
-${cde} keys:remove ${sshKeyId} 2>&1 | watchError | report "keys:add"
-# ${cde} keys:add ${sshFilePath}
+${cde} keys:remove ${sshKeyId} 2>&1 | watchError | report "keys:remove"
+
+# prepare stack data
+stackName="stackjava"
+${cde} stacks:create ${stackName} > /dev/null 2>&1
 git remote remove cde
-${cde} apps:create ${appName} javajersey-test 2>&1 | watchError | report "apps:create"
+${cde} apps:create ${appName} ${stackName} 2>&1 | watchError | report "apps:create"
 ${cde} apps:list 2>&1 | watchApp | report "apps:list"
 ${cde} apps:info -a ${appName} 2>&1 | watchApp | report "apps:info"
+
 ${cde} domains:add ${domainName} 2>&1 | watchError | report "domains:add"
 ${cde} domains:list 2>&1 | watchDomain | report "domains:list"
 ${cde} domains:remove ${domainName} 2>&1 | watchError | report "domains:remove"
+
 # prepare domain data
 domainName="domain.template.com"
 ${cde} domains:add ${domainName} > /dev/null 2>&1
-
 routeId=`${cde} routes:create ${domainName} ${routePath} 2>&1 | watchNewRoute `; echo $routeId | report "routes:create"
 ${cde} routes:list 2>&1 | watchRoutes | report "routes:list"
 if [[ ${routeId} == "Fail" ]]; then
   exit 1
 fi
 ${cde} routes:bind ${routeId} ${appName} | watchError | report "routes:bind"
+${cde} routes:unbind ${routeId} ${appName} | watchError | report "routes:unbind"
 
 # destroy resources
 # user unregister
