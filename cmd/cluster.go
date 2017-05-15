b36c0bda611214ab8607b19b8ed3a7b4020f9f6a
syzhang <syzhang@thoughtworks.com>
2017-05-15 23:22:18 +0800

    should show clusters list use cde command clusters:list

--- cmd/cluster.go
+++ cmd/cluster.go
@@ -1,7 +1,25 @@
 package cmd
 
-func ClusterList() error {
+import (
+	"fmt"
+	launcherApi "github.com/sjkyspa/stacks/launcher/api/api"
+	deploymentNet "github.com/sjkyspa/stacks/launcher/api/net"
+	"github.com/sjkyspa/stacks/client/config"
+)
 
+func ClusterList() error {
+	configRepository := config.NewConfigRepository(func(error) {})
+	clusterRepository := launcherApi.NewClusterRepository(configRepository, deploymentNet.NewCloudControllerGateway(configRepository))
+	clusters, err := clusterRepository.GetClusters()
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("=== Clusters [%d]\n", len(clusters.Items()))
+
+	for _, cluster := range clusters.Items() {
+		fmt.Printf("id: %d\n", cluster.Id())
+	}
 	return nil
 }
 
