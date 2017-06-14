40a96f57745aa6315f566365893228de3732a2bc
syzhang <syzhang@thoughtworks.com>
2017-06-14 11:45:25 +0800

    should set envs from baking service in runtime container

--- backend/compose/compose.go
+++ backend/compose/compose.go
@@ -70,6 +70,9 @@ func (cb ComposeBackend) ToComposeFile(s api.Stack) string {
 				fmt.Println(link)
 				env[fmt.Sprintf("%s_HOST", strings.ToUpper(link))] = link
 				env[fmt.Sprintf("%s_PORT", strings.ToUpper(link))] = fmt.Sprintf("%d", services[link].GetExpose()[0])
+				for envName, envValue := range services[link].GetEnv(){
+					env[fmt.Sprintf("%s_%s", strings.ToUpper(link), strings.ToUpper(envName))] = envValue
+				}
 			}
 
 			composeServices[name] = Service{
@@ -99,6 +102,9 @@ func (cb ComposeBackend) ToComposeFile(s api.Stack) string {
 			for _, link := range links {
 				env[fmt.Sprintf("%s_HOST", strings.ToUpper(link))] = link
 				env[fmt.Sprintf("%s_PORT", strings.ToUpper(link))] = fmt.Sprintf("%d", services[link].GetExpose()[0])
+				for envName, envValue := range services[link].GetEnv(){
+					env[fmt.Sprintf("%s_%s", strings.ToUpper(link), strings.ToUpper(envName))] = envValue
+				}
 			}
 
 			composeServices["runtime"] = Service{
